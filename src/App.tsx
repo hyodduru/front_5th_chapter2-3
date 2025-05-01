8f7ec60b5013a69282b5ca3df96dae08a86d363e
hyodduru <hoje15v@gmail.com>
2025-05-01 13:01:49 +0900

    feat: 라우팅 설정을 위해 RouterProvider 추가 및 애플리케이션 감쌈

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import { BrowserRouter as Router } from "react-router-dom"
-import Header from "./widgets/ui/Header.tsx"
-import Footer from "./widgets/ui/Footer.tsx"
-import PostsManagerPage from "./pages/PostsManagerPage.tsx"
+import { RouterProvider, ReactQueryProvider } from "./app/provider"
+import PostsManagerPage from "@pages/PostsManagerPage"
 
 const App = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <PostsManagerPage />
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <ReactQueryProvider>
+      <RouterProvider>
+        <PostsManagerPage />
+      </RouterProvider>
+    </ReactQueryProvider>
   )
 }
 
