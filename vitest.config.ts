8c31ab03e51c1132e1805a962c2f6aa492378a3f
hyodduru <hoje15v@gmail.com>
2025-05-01 11:10:00 +0900

    chore: tsconfig 경로 alias 설정 추가 및 정리

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,20 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
+import path from "path"
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@app": path.resolve(__dirname, "src/app"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@widgets": path.resolve(__dirname, "src/widgets"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@entities": path.resolve(__dirname, "src/entities"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
