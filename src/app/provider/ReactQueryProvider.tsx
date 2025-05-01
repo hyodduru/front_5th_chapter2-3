987d4cd0e713c7d928381232e924fe44195bad24
hyodduru <hoje15v@gmail.com>
2025-05-01 11:13:38 +0900

    feat: RouterProvider 및 ReactQueryProvider 생성

---
+++ src/app/provider/ReactQueryProvider.tsx
@@ -0,0 +1,8 @@
+import type { ReactNode } from "react"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
+
+export const ReactQueryProvider = ({ children }: { children: ReactNode }) => {
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+}
